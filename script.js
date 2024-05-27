a119a640779a9aa9b8ae8be9220836967bab335d
wassim mourali <wassim.mourali@rose-blanche.com>
2024-05-27 21:25:43 +0100

    avec commantaire

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+//gérer une ecoute sur evenement de fonction global
 document.addEventListener("DOMContentLoaded", function() {
+
+    //selectionner l'element HTML avec ID
     const color1 = document.getElementById("color-box");
     const changeColor = document.getElementById("change-color-btn");
     
+    //fonction qui gerer un couleur au hazard
     function getRandomColor() {
         const letters = '0123456789ABCDEF';
         let color = '#';
@@ -11,7 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
         return color;
     }
     
+    //gerer une evenement en ecoute
     changeColor.addEventListener("click", function() {
+
+        //mise à jour le coulour avec couleur au hazrd
         color1.style.backgroundColor = getRandomColor();
     });
 });
